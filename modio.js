971fd005256ada16f5e1d4c72ae77cf0af846c99
DodecaDev <105252647+DodecaTheDev@users.noreply.github.com>
2024-01-08 13:55:08 -0600

    Update modio.js

--- modio.js
+++ modio.js
@@ -1,26 +1,54 @@
-(function(Scratch) {
-  'use strict';
-
-  if (!Scratch.extensions.unsandboxed) {
-    throw new Error('This Hello World example must run unsandboxed');
-  }
-
-  class ModIo {
-    getInfo() {
-      return {
-        id: 'modio',
-        name: 'Mod.io',
-        blocks: [
-          {
-            opcode: 'hello',
-            blockType: Scratch.BlockType.REPORTER,
-            text: 'Hello!'
+class ColorExample {
+  getInfo() {
+    return {
+      id: 'colorexample',
+      name: 'Color Example',
+      // intentionally bad colors so that the effect is more clear
+      color1: '#ff0000', // pure red
+      color2: '#00ff00', // pure green
+      color3: '#0000ff', // pure blue
+      blocks: [
+        {
+          opcode: 'reporter',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'string [STRING] boolean [BOOLEAN] menu [MENU] field [FIELD]',
+          arguments: {
+            STRING: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: '1'
+            },
+            BOOLEAN: {
+              type: Scratch.ArgumentType.BOOLEAN
+            },
+            MENU: {
+              type: Scratch.ArgumentType.STRING,
+              menu: 'MENU'
+            },
+            FIELD: {
+              type: Scratch.ArgumentType.STRING,
+              menu: 'FIELD'
+            }
           }
-        ]
-      };
-    }
-    hello() {
-      return 'World!';
-    }
+        },
+      ],
+      menus: {
+        MENU: {
+          acceptReporters: true,
+          items: ['item 1', 'item 2']
+        },
+        // We're just including a field example for completion.
+        // Please do not use acceptReporters: false!
+        FIELD: {
+          acceptReporters: false,
+          items: ['item 1', 'item 2']
+        }
+      }
+    };
   }
-  Scratch.extensions.register(new ModIo());
+
+  reporter() {
+    return 'This block does nothing';
+  }
+}
+
+Scratch.extensions.register(new ColorExample());
