c784f3ca767a6112a5fd0e2027ef695c5028e75c
DodecaDev <105252647+DodecaTheDev@users.noreply.github.com>
2024-01-11 08:54:53 -0600

    Update modio.js

--- modio.js
+++ modio.js
@@ -1,46 +1,30 @@
-class ModIO {
-  getInfo() {
-    return {
+(function(Scratch) {
+  'use strict';
+
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error('This Hello World example must run unsandboxed');
+  }
+
+  class ModIO {
+    getInfo() {
+      return {
       id: "modio",
       name: "Mod.io",
       color1: "#1D5957",
       color2: "#163635",
       color3: "#163635",
-      blocks: [
-        {
-          opcode: "init",
-          blockType: Scratch.BlockType.COMMAND,
-          text: "initialize",
-        },
-        {
-          opcode: "getKey",
-          blockType: Scratch.BlockType.REPORTER,
-          text: "get api key",
-        },
-        {
-          opcode: "setKey",
-          blockType: Scratch.BlockType.COMMAND,
-          text: "set api key to [string]",
-          arguments: {
-            string: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: "api key",
-            },
-          },
-        },
-      ],
-    };
+        blocks: [
+          {
+            opcode: 'hello',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Hello!'
+          }
+        ]
+      };
+    }
+    hello() {
+      return 'World!';
+    }
   }
-  init() {
-    let apiKey,
-    apiKey = "api";
-  }
-  getKey() {
-    return apiKey;
-  }
-  setKey() {
-    apiKey = 'A';
-  }
-}
-
-Scratch.extensions.register(new ModIO());
+  Scratch.extensions.register(new ModIO());
+})(Scratch);
